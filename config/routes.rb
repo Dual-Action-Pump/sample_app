6d968bf50f516dc73be2e92becc1d8125da709b5
Dual-Action-Pump <mmorale8@binghamton.edu>
2017-07-09 00:27:21 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
 
-  get 'foo/baz'
+  root 'static_pages#home'
 
-  get 'static_pages/home'
+  get '/signup', to: 'users#new'
 
-  get 'static_pages/help'
+  get '/help',        to: 'static_pages#help'
 
-  get 'static_pages/about'
+  get '/about',       to: 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact',     to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+
 
 end
